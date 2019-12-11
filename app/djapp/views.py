e7ed07539b60ce326ea4a333d44e82e5fb6d3c74
webizfabulous <webizmoxx@gmail.com>
2019-12-11 18:13:00 +0545

    Added a new logic inside views where it will redirect me to about page after 4 refreshes

--- app/djapp/views.py
+++ app/djapp/views.py
@@ -6,7 +6,13 @@ import redis
 cache = redis.Redis(host='redis')
 
 tries = cache.set('tries' , 5)
+zero=cache.set('zero',0)
 
 def index(request):
+    decreaseTries = cache.decr('tries')
+    print(f"You will be redirected after {decreaseTries} refreshes")
+    if decreaseTries==zero:
+        return render(request , 'About.html')
     return render(request ,'home.html')
+
 
